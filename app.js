20b89d2f639aaba60dab266af2fb2310e961b35e
avi106 <avibhusal13@gmail.com>
2020-04-24 14:23:43 +1000

    generate random colors and check text contrast

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-//Global Selection and variables
+//Global Selection and Variables
 const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
-const sliders = document.querySelectorAll('input[type="range"');
+const sliders = document.querySelectorAll('input[type="range"]');
 const currentHex = document.querySelectorAll(".color h2");
-let initialColors;
+let initalColors;
 
 //Functions
-
 generateHex = () => {
   const hexColor = chroma.random();
   return hexColor;
@@ -14,13 +13,28 @@ generateHex = () => {
 
 randomColors = () => {
   colorDivs.forEach((div, index) => {
-    const HexText =  div.children[0];
+    const hexText = div.children[0];
     const randomColor = generateHex();
-   
-    //Add the color to the background
+
+    //Adding it to the background
     div.style.backgroundColor = randomColor;
-    HexText.innerText = randomColor;
+    hexText.innerText = randomColor;
+    //Check for contrast
+    checkTextContrast(randomColor, hexText);
+    //Initial Colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input')
+    console.log(sliders);
   });
+};
+
+checkTextContrast = (color, text) => {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 };
 
 randomColors();
